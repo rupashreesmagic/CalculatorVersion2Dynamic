ef89a9bcc2eafdc356ed7b6c91379a662541d9be
rupashreesmagic <rupashreesmagic@gmail.com>
2024-02-27 20:16:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+import ButtonsContainer from "./Components/ButtonsContainer";
+import Display from "./Components/Display";
+
+function App() {
+  const [calVal, setCalVal] = useState("");
+
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
+
+  return (
+    <div className={styles.calculator}>
+      <Display displayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+    </div>
+  );
+}
+
+export default App;
